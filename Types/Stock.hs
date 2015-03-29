4e610c84ae966e7935412e577ad92ff6cd9b0226
paperduck <paperduck@users.noreply.github.com>
2015-03-29 18:56:07 -0400

    Updated Types.Stock

--- Types/Stock.hs
+++ Types/Stock.hs
@@ -8,9 +8,13 @@
 -- <Value Constructor> can match <Type Name> if there is only one <Value Constructur>.
 --
 
---module Stock
---(
---) where
+-- If I wanted, I could choose to NOT export the value constructurs.
+-- This would prevent use of those functions, but those types could 
+-- be returned by an exposed function. Also, whoever uses the module can't
+-- pattern match against the value constructors.
+module Stock
+( Stock(..)
+) where
 
 
 -- Type Stock
