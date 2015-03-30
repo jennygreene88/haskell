2ddfe9b5c116656db3ba2c6ba598a6be03b9d386
akroner <akroner@codeblue.com>
2015-03-30 16:26:11 -0400

    Learning about records

--- Types/Stock.hs
+++ Types/Stock.hs
@@ -1,10 +1,11 @@
 --
--- data <Type Name> =
--- <Value Constructor> [<Field1> ... ]
--- [ | <Value Constructor> [<Field1> ... ] ] [ | ... ]
--- [ deriving (<Typeclass>) ]
+-- data <Data Type Name> =
+--     <Value Constructor> [<Field1> ... ]
+--     [ | <Value Constructor> [<Field1> ... ] [ | ... ] ... ] 
+--     [ deriving (<Typeclass>) ]
 --
--- <Type Name> must start with a capital letter
+-- <Data Type Name> must start with a capital letter
+-- <Value Constructor> must start with a capital letter
 -- <Value Constructor> can match <Type Name> if there is only one <Value Constructur>.
 --
 
@@ -12,14 +13,15 @@
 -- This would prevent use of those functions, but those types could 
 -- be returned by an exposed function. Also, whoever uses the module can't
 -- pattern match against the value constructors.
-module Stock
-( Stock(..)
-) where
+--
+--module Stock
+--( Stock(..)
+--) where
 
-
--- Type Stock
--- Price <datetime> <price for that timestamp>
--- <stock symbol>
--- Max <max price ever>
--- Min <min price ever>
 data Stock = Price String Float | Symbol String | Max Float | Min Float deriving (Show)
+
+-- get specific info
+get_symbol :: Stock -> String
+get_symbol (Symbol s) = s
+
+
